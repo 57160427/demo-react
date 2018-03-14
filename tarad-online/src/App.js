73d930b6031e72347410803e138838fbea300daa
remrin69 <puwanart.tin@gmail.com>
2018-03-14 23:47:49 +0700

    Config route

--- tarad-online/src/App.js
+++ tarad-online/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+// Styles
+// Import Main styles for this application
+import './App.css'
+
+// Import components
+import Index from './views/index'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <HashRouter>
+                    <Switch>
+                        <Route path="/" name="index" component={Index} />
+                    </Switch>
+                </HashRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
