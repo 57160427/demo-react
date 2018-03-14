bcbfc197ced94e01d3a4bdd72b80eaf5e0a99ab1
remrin69 <puwanart.tin@gmail.com>
2018-03-14 23:48:43 +0700

    Initial Index component

--- tarad-online/src/views/index/index.js
+++ tarad-online/src/views/index/index.js
@@ -1,1 +1,11 @@
 import React, { Component } from 'react'
+
+class Index extends Component {
+    render() {
+        return (
+            <h1>Tarad Online</h1>
+        )
+    }
+}
+
+export default Index
