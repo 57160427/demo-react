d9985272ca9384afe779ab900d033f09bf1dbfcd
remrin69 <puwanart.tin@gmail.com>
2018-03-14 23:31:04 +0700

    Initial Full component

--- tarad-online/src/container/Full/Full.js
+++ tarad-online/src/container/Full/Full.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react'
-import {} from 'react-router-dom'
+import React, { Component } from 'react'
+import { } from 'react-router-dom'
 
 class Full extends Component {
     render() {
-        
+
     }
 }
